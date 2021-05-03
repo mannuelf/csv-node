08d4cc98a40affb00f718f7256ee143b1e0b0616
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-03 05:39:43 +0200

    convert values in the csv rows to correct type, use type assertion to overide ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './matchResult';
 
 const reader = new CsvFileReader('football.csv');
 
 reader.read();
-
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
 
 let manUnitedWins = 0;
 
