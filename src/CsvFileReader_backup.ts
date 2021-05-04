3b58e45e098982a7516f411ee107d31c97702ccf
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-04 05:37:58 +0200

    CsvFileReader is abstract class

--- src/CsvFileReader_backup.ts
+++ src/CsvFileReader_backup.ts
@@ -1,16 +1,13 @@
 import fs from 'fs';
+import { dateStringToDate } from './utils';
 import { MatchResult } from './matchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
-// abstract class, cannot be used to create an instance of an object
-// abstract methods must be implimented by a child class
-export abstract class CsvFileReader {
+export class CsvFileReader {
   data: MatchData[] = [];
 
   constructor(public filename: string) {}
-
-  abstract mapRow(row: string[]): MatchData;
 
   read(): void {
     this.data = fs
@@ -21,7 +18,18 @@ export abstract class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(',');
       })
-      .map(this.mapRow);
+      .map(
+        (row: string[]): MatchData => {
+          return [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,
+            row[6],
+          ];
+        }
+      );
   }
-
 }
