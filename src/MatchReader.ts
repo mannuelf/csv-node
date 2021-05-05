70c7d856db006df0a274533ec7c60c86ef5befb1
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-05 07:39:18 +0200

    composition v1

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,7 +1,6 @@
 import { dateStringToDate } from './utils';
 import { MatchResult } from './matchResult';
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
+import { MatchData } from './MatchData';
 
 interface DataReader {
   read(): void;
