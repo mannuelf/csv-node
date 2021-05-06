c2e4421460ce6fe71fd35b846db6bc0e0df99380
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-06 15:45:39 +0200

    generics e.g.

--- src/generics.ts
+++ src/generics.ts
@@ -38,3 +38,30 @@ printAnything<number>([1, 2, 3, 4]);
 // type inference, dont specifically have to annotate the type,
 // but is helpful to have as above.
 printAnything([0, 1, 2, 3, 4]);
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+// interface, contract or contraint
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<House>([new House(), new House()]);
+
+printHousesOrCars<Car>([new Car()]);
